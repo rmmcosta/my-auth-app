a1a11e4d754af627de4834ce5ae8dce331c43c19
Ana Margarida Dias <ricardocosta101085@gmail.com>
2024-10-27 17:30:51 +0000

    feat:get basic resource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly apiUrl = "http://localhost:8080"
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get(`${this.apiUrl}/resource`).subscribe(data => {
+      console.log(`resource data: ${JSON.stringify(data)}`);
+      this.greeting = data;
+    });
+  }
   title = 'my-auth-app';
+  greeting: any = { 'id': 'XXX', 'message': 'Hello unknown' }
 }
